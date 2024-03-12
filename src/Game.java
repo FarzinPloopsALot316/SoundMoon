4e22832220629f912dbb42f9bc10ec0dc2f235a8
BT_1N3_07 <no@bths.edu>
2024-03-12 09:27:51 -0400

    game added

---
+++ src/Game.java
@@ -0,0 +1,6 @@
+public class Game {
+    public void start () {
+        System.out.println("Welcome to Sound Moon!");
+        System.out.println("Please wait while we load the game file...");
+    }
+}
--- src/Gun.java
+++ src/Gun.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class Gun {
+public class Gun extends Weapons{
 }
