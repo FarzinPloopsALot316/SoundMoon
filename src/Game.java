de32c79cf5de819e0c410981a08ed0f6bcccec2b
Farzin Riham <friham07@Gmail.com>
2024-03-12 20:59:00 -0400

    ConsoleUtility, src classes added, Gun class removed.

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,36 @@
 public class Game {
+    public void wait(int seconds) {
+        seconds *= 1000;
+        try {
+            Thread.sleep(seconds);
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+    }
+
+    public void wait2seconds() {
+        try {
+            Thread.sleep(2000);  // 2000 milliseconds, or 2 seconds
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+    }
+
+    public void waitASecond() {
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+    }
     public void start () {
-        System.out.println("Welcome to Sound Moon!");
-        System.out.println("Please wait while we load the game file...");
+        System.out.println(ConsoleUtility.PURPLE + "Welcome to Sound Moon!");
+        System.out.println(ConsoleUtility.RED + "WARNING: DISTURBING CONTENT IS PRESENT, PLAY WITH CAUTION.");
+        System.out.println(ConsoleUtility.RED + "PLEASE PLAY THE GAME IN THE TERMINAL FOR THE BEST EXPERIENCE.");
+        wait(5);
+        System.out.println(ConsoleUtility.PURPLE + "Remember to have fun! Starting game...");
+        wait(3);
+        ConsoleUtility.clearScreen();
+        System.out.println(ConsoleUtility.PURPLE + "oh boo hoo vampires and werewolves");
     }
 }
