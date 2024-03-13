e2a0fe8321748253f21532c59e6800b944a18311
Farzin Riham <friham07@Gmail.com>
2024-03-12 22:41:26 -0400

    Inventory class updated, voltageSword.

--- src/Inventory.java
+++ src/Inventory.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 
 public class Inventory extends Game {
     private ArrayList<Weapons> weaponsInv; // this also takes in sword as a subclass to weapons
-    private String[] foodInv; // players can have a max of only 3 food/healing objects.
+    private String[] foodInv; // players can have a max of only 2 food/healing objects.
+
+    private Sword voltageSword;
 
     public Inventory () {
-        this.foodInv = new String[3];
+        this.foodInv = new String[2];
         this.weaponsInv = new ArrayList<>();
-        Sword voltageSword = new Sword("Lancer", 10);
+        this.voltageSword = new Sword("Lancer", 10);
         weaponsInv.add(voltageSword);
-
     }
-
 }
