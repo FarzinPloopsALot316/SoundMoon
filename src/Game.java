a653efdc2334fbf57ccae51a996a9c67d8fa5793
Farzin Riham <friham07@Gmail.com>
2024-04-07 13:50:52 -0400

    printCastleMaze2

--- src/Game.java
+++ src/Game.java
@@ -1995,7 +1995,35 @@ public class Game {
         }
     }
 
-
+    public void printCastleMaze2() {
+        /* this is how it should be printed!
+        |||O|||
+        |||||||
+        |||||||
+        |||||||
+        $___|||
+        |||X|||
+        |||_|||
+        */
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze.length; j++) {
+                maze[i][j] = new Space("|");
+            }
+        }
+        maze[5][3] = new Space("X");
+        maze[4][0] = new Space("$");
+        maze[0][3] = new Space("O");
+        maze[6][3] = new Space("_");
+        maze[4][3] = new Space("_");
+        maze[4][2] = new Space("_");
+        maze[4][1] = new Space("_");
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze.length; j++) {
+                System.out.print(maze[i][j].getSymbol());
+            }
+            System.out.println();
+        }
+    }
 
     private boolean navigate (int row, int col) {
         Scanner choice = new Scanner(System.in);
